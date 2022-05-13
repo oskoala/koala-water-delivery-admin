cd4483cc098d1071ff6a07cb8ca6bf08f0df0694
koala <koala@koala.com>
2022-05-13 14:43:59 +0800

    第一章节 三个测试接口

---
+++ routes/api.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::get("test", function (Request $request) {
+    return Response::json([
+        "code" => 0,
+        "msg"  => "请求成功",
+        "data" => [
+            "sentence" => "Hello Laravel !"
+        ]
+    ]);
+});
+
+Route::get("test_get", function (Request $request) {
+    return Response::json([
+        "code" => 0,
+        "msg"  => "请求成功",
+        "data" => $request->input()
+    ]);
+});
+
+
+Route::post("test_post", function (Request $request) {
+    return Response::json([
+        "code" => 0,
+        "msg"  => "请求成功",
+        "data" => $request->input()
+    ]);
+});
