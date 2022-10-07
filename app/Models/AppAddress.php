288c825aea029245dfdbbdab60ff8bc1c3e9b71c
mmx <657873584@qq.com>
2022-10-07 11:08:18 +0800

    检测是否超出配送范围

--- app/Models/AppAddress.php
+++ app/Models/AppAddress.php
@@ -21,4 +21,23 @@ class AppAddress extends Model
         "lat",
         "lon",
     ];
+
+    protected $appends = [
+        'out_deliver_scope',// 标记是否超出配送范围
+    ];
+
+    public function getOutDeliverScopeAttribute()
+    {
+        // 计算是否超出配送范围
+        $limit_deliver_scope = custom_config("deliver_scope");
+        $shop_lat            = custom_config("shop_lat");
+        $shop_lon            = custom_config("shop_lon");
+
+        if ($this->lon && $this->lat) {
+            if (cal_distance($shop_lat, $shop_lon, $this->lat, $this->lon) <= $limit_deliver_scope) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
